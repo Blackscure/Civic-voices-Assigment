fdf7950ccce401c7287cb762b56e54decfdbd7b4
Blackscure <34754398+Blackscure@users.noreply.github.com>
2025-01-21 00:55:34 +0300

    code cleaning

--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -3,17 +3,144 @@ const {
     getTodos,
     createTodo,
     updateTodo,
-    deleteTodo
+    deleteTodo,
 } = require('../controllers/todoController');
 const { verifyToken } = require('../middleware/authMiddleware'); 
 
 const router = express.Router();
 
-router.use(verifyToken); 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Todo:
+ *       type: object
+ *       properties:
+ *         title:
+ *           type: string
+ *           description: The title of the todo
+ *       required:
+ *         - title
+ */
 
-router.get('/', getTodos); 
-router.post('/', createTodo); 
-router.put('/:id', updateTodo); 
-router.delete('/:id', deleteTodo); 
+/**
+ * @swagger
+ * tags:
+ *   name: Todos
+ *   description: API endpoints for managing todos
+ */
+
+// Middleware for token verification
+router.use(verifyToken);
+
+/**
+ * @swagger
+ * /todos:
+ *   get:
+ *     summary: Retrieve all todos
+ *     tags: [Todos]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: List of all todos
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Todo'
+ *       401:
+ *         description: Unauthorized
+ */
+router.get('/', getTodos);
+
+/**
+ * @swagger
+ * /todos:
+ *   post:
+ *     summary: Create a new todo
+ *     tags: [Todos]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Todo'
+ *     responses:
+ *       201:
+ *         description: Todo created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Todo'
+ *       400:
+ *         description: Bad request
+ *       401:
+ *         description: Unauthorized
+ */
+router.post('/', createTodo);
+
+/**
+ * @swagger
+ * /todos/{id}:
+ *   put:
+ *     summary: Update an existing todo
+ *     tags: [Todos]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The ID of the todo to update
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Todo'
+ *     responses:
+ *       200:
+ *         description: Todo updated successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Todo'
+ *       404:
+ *         description: Todo not found
+ *       401:
+ *         description: Unauthorized
+ */
+router.put('/:id', updateTodo);
+
+/**
+ * @swagger
+ * /todos/{id}:
+ *   delete:
+ *     summary: Delete a todo
+ *     tags: [Todos]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The ID of the todo to delete
+ *     responses:
+ *       200:
+ *         description: Todo deleted successfully
+ *       404:
+ *         description: Todo not found
+ *       401:
+ *         description: Unauthorized
+ */
+router.delete('/:id', deleteTodo);
 
 module.exports = router;
