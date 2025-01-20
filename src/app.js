b0b151d09dbaad18bdde24b0e8b347fabf4c95e8
Blackscure <34754398+Blackscure@users.noreply.github.com>
2025-01-20 23:13:05 +0300

    routes add .

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(corsMiddleware);
 app.use(express.json());
 
-
-
+app.use('/apps/todo-list/api/v1/authentication', authRoutes);
+app.use('/apps/todo-list/api/v1/todos', todoRoutes);
 
 module.exports = app;
